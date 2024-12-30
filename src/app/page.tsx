8b2660bb867db2868b7a609ce6d81bfe87ab9f3a
kietthetran123@gmail.com <kiet.tt@vgamestd.com>
2024-12-30 22:24:50 +0700

    update hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 "use client";
 import { SidebarProvider } from "@/providers/side-bar.provider";
+import Base from "@/templates/Base";
 
 export default function Home() {
 
   return (
-    <main className="flex fixed w-full">
-      Hi
-    </main>
+    <div className="text-gray-600 antialiased">
+      <Base/>
+    </div>
   );
 }
