b273d1133abba98f7b3af54c95e37e151846ccbb
tin.nd <tin.nd@vgamestd.com>
2025-01-01 01:48:45 +0700

    feat: component trade page

--- src/app/trade/page.tsx
+++ src/app/trade/page.tsx
@@ -1,9 +1,24 @@
+import DexChart from '@/components/dex-chart'
+import FormTrade from '@/components/form-trade'
+import InformationToken from '@/components/information-token'
 import WrapTradeComponent from '@/components/wrap-trade'
 import React from 'react'
 
 const Trade = () => {
   return (
-    <WrapTradeComponent>trade</WrapTradeComponent>
+    <WrapTradeComponent>
+      <section className='w-full lg:max-w-[1400px] mx-auto grid grid-cols-3 gap-4 py-10 md:py-[62px]'>
+        <div className='col-span-2'>
+          <div className='grid grid-cols-1'>
+            <DexChart />
+            <InformationToken />
+          </div>
+        </div>
+        <div className='col-span-1'>
+          <FormTrade />
+        </div>
+      </section>
+    </WrapTradeComponent>
   )
 }
 
