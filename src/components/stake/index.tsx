1cbaade398c720f2e08b761b0f01d6374f859171
huuquan2952001 <huuquan2952001@gmail.com>
2025-01-01 11:13:33 +0700

    feat: add UI reward, position

--- src/components/stake/index.tsx
+++ src/components/stake/index.tsx
@@ -8,7 +8,7 @@ const StakePage = () => {
   const [activeTab, setActiveTab] = useState("stake");
   return (
     <div className="w-full justify-center flex">
-      <div className="flex flex-col md:gap-[49px] gap-[24px] md:pt-[32px] pt-[26px] md:px-[40px] px-[8px] w-full max-w-[1440px] md:pb-0 pb-[24px] items-center ">
+      <div className="flex flex-col md:gap-[49px] gap-[24px] md:pt-[32px] pt-[26px] md:px-[40px] px-[8px] w-full max-w-[1440px] pb-[24px] items-center">
         <div className="w-full md:max-w-[406px] relative md:block flex flex-col md:gap-0 gap-[24px]">
           <div className="w-full flex flex-col gap-[24px] md:items-start items-center">
             <StakeTab setActiveTab={setActiveTab} activeTab={activeTab} />
